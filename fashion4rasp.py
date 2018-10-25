15efdb11e3cf6688c16a36aa60efdfebe1368e33
miniBear <ax_zaa@windowslive.com>
2018-10-26 05:05:39 +0700

    new commit

--- fashion4rasp.py
+++ fashion4rasp.py
@@ -1,7 +1,11 @@
 import numpy as np
 import tensorflow as tf
-import tensorflow.keras as keras
-from tensorflow.keras.models import model_from_json
+# import tensorflow.keras as keras
+import keras
+from keras.models import load_model, model_from_json
+# from tensorflow.keras.models import model_from_json, load_model
+
+
 import matplotlib.pyplot as plt
 
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
@@ -15,14 +19,30 @@ test_images = test_images / 255.0
 train_images = train_images.reshape(60000, 28, 28, 1)
 test_images = test_images.reshape(10000, 28, 28, 1)
 
-# load json and create model
-json_file = open('/home/pi/model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-loaded_model = model_from_json(loaded_model_json)
-# load weights into new model
-loaded_model.load_weights("/home/pi/model.h5")
-print("Loaded model from disk")
+try:
+    # # load json and create model
+    # json_file = open('/home/pi/model.json', 'r')
+    # loaded_model_json = json_file.read()
+    # json_file.close()
+    # loaded_model = model_from_json(loaded_model_json)
+    # # load weights into new model
+    # loaded_model.load_weights("/home/pi/model.h5")
+    # print("Loaded model from disk")
+
+    # # load json and create model
+    # json_file = open('model.json', 'r')
+    # loaded_model_json = json_file.read()
+    # json_file.close()
+    # loaded_model = model_from_json(loaded_model_json)
+    # # load weights into new model
+    # loaded_model.load_weights("model.h5")
+    # print("Loaded model from disk")
+
+    loaded_model = load_model("/home/pi/model.h5")
+    print("Loaded model from disk")
+except:
+    loaded_model = load_model("model.h5")
+    print("Loaded model from disk")
 
 loaded_model.compile(optimizer=tf.train.AdamOptimizer(),
               loss='sparse_categorical_crossentropy',
